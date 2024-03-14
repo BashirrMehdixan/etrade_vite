2901fb4581afe78ce71c44fd23858b3224bf66ec
Bashirr <beshirr096@gmail.com>
2024-03-14 15:48:38 +0400

    filter completed and Proxima Nova font added

--- src/pages/about/AboutCompany.jsx
+++ src/pages/about/AboutCompany.jsx
@@ -1,3 +1,5 @@
+import { FaShoppingBasket } from "react-icons/fa"
+
 import "./css/AboutCompany.css"
 
 const AboutCompany = () => {
@@ -7,8 +9,21 @@ const AboutCompany = () => {
                 <div className="container">
                     <div className="about-box">
                         <div className="left-side">
+                            <img src={'assets/images/about/about-01.png'} alt="" />
                         </div>
-                        <div className="right-side"></div>
+                        <div className="right-side">
+                            <div className="head-box products-head">
+                                <p className="best-deal">
+                                    <span>
+                                        <FaShoppingBasket />
+                                    </span>
+                                    Our products
+                                </p>
+                                <p className="uni-head head1">
+                                    Explore our Products
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
